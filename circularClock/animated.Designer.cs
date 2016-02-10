0f8c5ba7fea5f568a221b112ba6fe0d3ce124206
Bibhuti <bibhutipd@gmail.com>
2016-02-10 23:01:18 +0545

    initial commit

---
+++ circularClock/animated.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace ShutDownTimer
+{
+    partial class animated
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(animated));
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.lblTime = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.changeColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pinkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.blueToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.brownToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.purpleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.greenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tomatoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showInDesktopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // lblTime
+            // 
+            this.lblTime.BackColor = System.Drawing.Color.White;
+            this.lblTime.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.ForeColor = System.Drawing.Color.Black;
+            this.lblTime.Location = new System.Drawing.Point(55, 93);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(117, 32);
+            this.lblTime.TabIndex = 0;
+            this.lblTime.Text = "08:17 AM";
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Circular Clock";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem,
+            this.changeColorToolStripMenuItem,
+            this.showInDesktopToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(173, 70);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // changeColorToolStripMenuItem
+            // 
+            this.changeColorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pinkToolStripMenuItem,
+            this.blueToolStripMenuItem,
+            this.brownToolStripMenuItem,
+            this.purpleToolStripMenuItem,
+            this.greenToolStripMenuItem,
+            this.tomatoToolStripMenuItem});
+            this.changeColorToolStripMenuItem.Name = "changeColorToolStripMenuItem";
+            this.changeColorToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.changeColorToolStripMenuItem.Text = "Change Color";
+            // 
+            // pinkToolStripMenuItem
+            // 
+            this.pinkToolStripMenuItem.Name = "pinkToolStripMenuItem";
+            this.pinkToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
+            this.pinkToolStripMenuItem.Text = "Pink";
+            this.pinkToolStripMenuItem.Click += new System.EventHandler(this.pinkToolStripMenuItem_Click);
+            // 
+            // blueToolStripMenuItem
+            // 
+            this.blueToolStripMenuItem.Name = "blueToolStripMenuItem";
+            this.blueToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
+            this.blueToolStripMenuItem.Text = "Blue";
+            this.blueToolStripMenuItem.Click += new System.EventHandler(this.pinkToolStripMenuItem_Click);
+            // 
+            // brownToolStripMenuItem
+            // 
+            this.brownToolStripMenuItem.Name = "brownToolStripMenuItem";
+            this.brownToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
+            this.brownToolStripMenuItem.Text = "Brown";
+            this.brownToolStripMenuItem.Click += new System.EventHandler(this.pinkToolStripMenuItem_Click);
+            // 
+            // purpleToolStripMenuItem
+            // 
+            this.purpleToolStripMenuItem.Name = "purpleToolStripMenuItem";
+            this.purpleToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
+            this.purpleToolStripMenuItem.Text = "Purple";
+            this.purpleToolStripMenuItem.Click += new System.EventHandler(this.pinkToolStripMenuItem_Click);
+            // 
+            // greenToolStripMenuItem
+            // 
+            this.greenToolStripMenuItem.Name = "greenToolStripMenuItem";
+            this.greenToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
+            this.greenToolStripMenuItem.Text = "Green";
+            this.greenToolStripMenuItem.Click += new System.EventHandler(this.pinkToolStripMenuItem_Click);
+            // 
+            // tomatoToolStripMenuItem
+            // 
+            this.tomatoToolStripMenuItem.Name = "tomatoToolStripMenuItem";
+            this.tomatoToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
+            this.tomatoToolStripMenuItem.Text = "Tomato";
+            this.tomatoToolStripMenuItem.Click += new System.EventHandler(this.pinkToolStripMenuItem_Click);
+            // 
+            // showInDesktopToolStripMenuItem
+            // 
+            this.showInDesktopToolStripMenuItem.Name = "showInDesktopToolStripMenuItem";
+            this.showInDesktopToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.showInDesktopToolStripMenuItem.Text = "Show in desktop";
+            this.showInDesktopToolStripMenuItem.Click += new System.EventHandler(this.showInDesktopToolStripMenuItem_Click);
+            // 
+            // animated
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Purple;
+            this.ClientSize = new System.Drawing.Size(260, 260);
+            this.Controls.Add(this.lblTime);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(330, 345);
+            this.Name = "animated";
+            this.ShowInTaskbar = false;
+            this.Text = "Circular Clock";
+            this.TransparencyKey = System.Drawing.Color.Purple;
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.animated_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.animated_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.animated_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.animated_MouseUp);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem changeColorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pinkToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem blueToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem brownToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem purpleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem greenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tomatoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showInDesktopToolStripMenuItem;
+    }
+}
---
+++ circularClock/animated.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShutDownTimer
+{
+    public partial class animated : Form
+    {
+        public animated()
+        {
+            InitializeComponent();
+            g = this.CreateGraphics();
+            timer.Start();
+            setup();
+        }
+
+        Graphics g;
+        Brush bM;
+        Brush bH;
+        Brush bS;
+        int rH, rM, rS,lH,lM,lS,cH,cM,cS,sH,sM,sS = 0;
+        DateTime currentTime;
+        void setup()
+        {
+
+            setColor(Clock.Properties.Settings.Default.clockColor);
+
+            //radius
+            rH = 250;
+            rM = 210;
+            rS = 160;
+
+            //location
+            lH = 5;
+            lM = 25;
+            lS = 50;
+
+            //circumference
+            cH =Convert.ToInt32(2 * Math.PI * rH);
+            cM = Convert.ToInt32(2 * Math.PI * rM);
+            cS = Convert.ToInt32(2 * Math.PI * rS);
+
+            //sweep angle
+            sH = cH / 60;
+            sM = cM / 60;
+            sS = cS / 60;
+
+            //fix time location
+            lblTime.Location = new Point(this.Width / 2 - lblTime.Width / 2 +5, this.Height / 2 - lblTime.Height / 2 +5);
+
+        }
+
+        void setColor(string color)
+        {
+
+            if (color == "pink")
+            {
+                bH = Brushes.Crimson;
+                bM = Brushes.HotPink;
+                bS = Brushes.LightPink;
+            }
+
+            else if (color == "blue")
+            {
+                bH = Brushes.RoyalBlue;
+                bM = Brushes.DodgerBlue;
+                bS = Brushes.DeepSkyBlue;
+            }
+
+            else if (color == "tomato")
+            {
+                bH = Brushes.Tomato;
+                bM = Brushes.Salmon;
+                bS = Brushes.LightSalmon;
+            }
+
+            else if (color == "brown")
+            {
+                bH = Brushes.SaddleBrown;
+                bM = Brushes.Chocolate;
+                bS = Brushes.SandyBrown;
+            }
+
+            else if (color == "green")
+            {
+                bH = Brushes.ForestGreen;
+                bM = Brushes.LimeGreen;
+                bS = Brushes.LightGreen;
+            }
+
+            else if (color == "purple")
+            {
+                bH = Brushes.DarkOrchid;
+                bM = Brushes.MediumOrchid;
+                bS = Brushes.Plum;
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            currentTime = DateTime.Now;
+            sS = int.Parse(DateTime.Now.ToString("ss")) *6;
+            sM = int.Parse(DateTime.Now.ToString("mm")) * 6;
+
+            if (currentTime.ToString("tt") == "PM")
+            {
+                sH = int.Parse(DateTime.Now.ToString("hh")) * 50;
+            }
+
+            else 
+            {
+                sH = int.Parse(DateTime.Now.ToString("hh")) * 6;
+            }
+
+
+
+            lblTime.Text = currentTime.ToShortTimeString();
+
+            this.Refresh();
+
+        }
+
+        private void animated_Paint(object sender, PaintEventArgs e)
+        {
+            //white borderrrrrrr
+            e.Graphics.FillPie(Brushes.WhiteSmoke, new Rectangle(lH-5, lH-5, rH+10, rH+10), 0, 360);
+           
+            //hour
+            e.Graphics.FillPie(Brushes.Silver, new Rectangle(lH, lH, rH, rH), 0, 360);//back
+            e.Graphics.FillPie(bH, new Rectangle(lH,lH,rH,rH),0,sH);
+
+            //minute
+            e.Graphics.FillPie(Brushes.LightGray, new Rectangle(lM, lM, rM, rM), 0, 360);//back
+            e.Graphics.FillPie(bM, new Rectangle(lM, lM, rM, rM), 0, sM);
+
+            //second
+            e.Graphics.FillPie(Brushes.Gainsboro, new Rectangle(lS, lS, rS, rS), 0, 360);//back
+            e.Graphics.FillPie(bS, new Rectangle(lS, lS, rS, rS), 0, sS);
+
+
+            //at centre
+            e.Graphics.FillPie(Brushes.White, new Rectangle(lS + 10, lS + 10, rS - 20, rS - 20), 0, 360);
+           
+           
+          
+        }
+
+        int x, y = 0;
+        bool down = false;
+        private void animated_MouseDown(object sender, MouseEventArgs e)
+        {
+            down = true;
+            x = e.X;
+            y = e.Y;
+        }
+
+        private void animated_MouseUp(object sender, MouseEventArgs e)
+        {
+            down = false;
+        }
+
+        private void animated_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (down)
+            {
+                this.Location = new Point(Cursor.Position.X - x, Cursor.Position.Y - y);
+            }
+        }
+
+        private void pinkToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem tt = (ToolStripMenuItem)sender;
+            setColor(tt.Text.ToLower());
+            Clock.Properties.Settings.Default.clockColor = tt.Text.ToLower();
+            Clock.Properties.Settings.Default.Save();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void showInDesktopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.TopMost = true;
+            this.TopMost = false;
+        }
+    }
+}
